c526ee602f737334c9f7d521476852ce85223991
AlexTheBestCoder <thebestcoder10@gmail.com>
2023-05-26 17:09:13 +0000

    V1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Home from './views/Home'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function App() {
   return (
     <div className='App'>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   )
 }
